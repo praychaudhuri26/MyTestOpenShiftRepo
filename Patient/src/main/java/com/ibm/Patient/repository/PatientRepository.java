71ca241eb57dae0c93755f25d58f9bbb66bed0de
pradiproychowdhury <pradiproychowdhury@localhost>
2020-04-11 19:32:36 +0530

    My First Commit for OpenShit testing

---
+++ Patient/src/main/java/com/ibm/Patient/repository/PatientRepository.java
@@ -0,0 +1,10 @@
+package com.ibm.Patient.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ibm.Patient.model.Patient;
+
+@Repository
+public interface PatientRepository extends MongoRepository<Patient, String> {
+}
